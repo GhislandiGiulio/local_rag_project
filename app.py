1bcafdd5585e64ce46e5662905561a42b5e8011a
GhislandiGiulio <giulio.ghislandi@itsrizzoli.it>
2025-02-07 14:34:49 +0100

    implemented correct page retention after scraping

--- app.py
+++ app.py
@@ -35,10 +35,10 @@ if st.session_state.file_uploaded and not st.session_state.file_scraped:
             st.session_state.file_scraped = True
             
             with st.spinner("Processing..."):
-                paragraphs, num_pages = scrape_pdf(file)
+                paragraphs_with_pages, num_pages = scrape_pdf(file)
                 
             with st.spinner("Embedding and uploading to DB..."):
-                st.session_state.embedder.embed_and_load(paragraphs=paragraphs, num_pages=num_pages, collection_name=file.name)
+                st.session_state.embedder.embed_and_load(paragraphs_with_pages=paragraphs_with_pages, num_pages=num_pages, collection_name=file.name)
                 
             analyze_placeholder.empty()  # Clear the button after processing
 
@@ -52,9 +52,9 @@ if st.session_state.file_scraped:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
-        # Placeholder AI response (replace with your logic)
-        query_result = st.session_state.embedder.search(user_input)
-        response = f"You can find out more about your question at page: {query_result}"
+        # search in vector db through query
+        query_result = st.session_state.embedder.search(user_input, collection_name=file.name)
+        response = f"You can find out more about your question at pages: \n{' '.join(['- ' + str(page) + '\n' for page in query_result])}"
 
         # Append AI response to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
